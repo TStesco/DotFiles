3a05febab89d40ce936e63bdcba31dce638831a2
tom.stesco@gmail.com <tom.stesco@gmail.com>
2018-09-10 23:35:20 +0200

    changing euler tmuxinator config

--- rsync_src_to_dst.sh
+++ rsync_src_to_dst.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# this script adds exclude statements to rsync for DotFiles repo
+# script adds exclude statements to rsync for DotFiles repo
 DOTFILES_SOURCE="$HOME/projects/DotFiles/"
 DOTFILES_DEST="$HOME"
-shopt -s dotglobs
 rsync -va --exclude ".git" --exclude "LICENSE" --exclude "README.md" \
-    --exclude ".gitignore" --exclude "rsync_src_dst.sh" \
+    --exclude ".gitignore" --exclude "rsync_src_to_dst.sh" \
     "$DOTFILES_SOURCE" "$DOTFILES_DEST"
